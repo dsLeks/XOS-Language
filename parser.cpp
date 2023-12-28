6745567653f895bbbfe176b1edd364c994e545f5
dsLeks <alekhyahello@gmail.com>
2023-12-27 18:05:31 -0800

    Result resturn type for parserOut

--- parser.cpp
+++ parser.cpp
@@ -12,47 +12,39 @@
 
 #include "xos.h"
 
-namespace xos
-{
+namespace xos {
 
-  Result<std::unique_ptr<ast::Str>> Parser::parseStr()
-  {
-    Result<Token> res = lexer_.Lex();
-    if (res.hasError())
-    {
-      return res;
-    }
-    const Token &tok = res.get();
-    if (tok.getKind() != Token::string)
-    {
-      return Result<std::unique_ptr<ast::Str>>::BuildError()
-             << "Expected string on row " << tok.getRow() << ", col "
-             << tok.getCol();
-    }
-    return Result<std::unique_ptr<ast::Str>>(
-        std::make_unique<ast::Str>(tok.getVal()));
+Result<std::unique_ptr<ast::Str>> Parser::parseStr() {
+  Result<Token> res = lexer_.Lex();
+  if (res.hasError()) {
+    return res;
+  }
+  const Token &tok = res.get();
+  if (tok.getKind() != Token::string) {
+    return Result<std::unique_ptr<ast::Str>>::BuildError()
+           << "Expected string on row " << tok.getRow() << ", col "
+           << tok.getCol();
+  }
+  return Result<std::unique_ptr<ast::Str>>(
+      std::make_unique<ast::Str>(tok.getVal()));
+}
+
+Result<std::unique_ptr<ast::Out>> Parser::parseOut() {
+  Result<Token> res = lexer_.Lex();
+  if (res.hasError()) return res;
+
+  const Token &tok = res.get();
+  if (tok.getKind() != Token::out) {
+    return Result<std::unique_ptr<ast::Out>>::BuildError()
+           << "Expected 'out' on row " << tok.getRow() << ", col "
+           << tok.getCol();
   }
 
-  Result<std::unique_ptr<ast::Out>> Parser::parseOut()
-  {
-    Result<Token> res = lexer_.Lex();
-    if (res.hasError())
-      return res;
+  Result<std::unique_ptr<ast::Str>> sres = parseStr();
+  if (sres.hasError()) return sres;
+  std::unique_ptr<ast::Str> &str = sres.get();
+  return Result<std::unique_ptr<ast::Out>>(
+      std::make_unique<ast::Out>(std::move(str)));
+}
 
-    const Token &tok = res.get();
-    if (tok.getKind() != Token::out)
-    {
-      return Result<std::unique_ptr<ast::Out>>::BuildError()
-             << "Expected 'out' on row " << tok.getRow() << ", col "
-             << tok.getCol();
-    }
-
-    Result<std::unique_ptr<ast::Str>> sres = parseStr();
-    if (sres.hasError())
-      return sres;
-    std::unique_ptr<ast::Str> &str = sres.get();
-    return Result<std::unique_ptr<ast::Out>>(
-        std::make_unique<ast::Out>(std::move(str)));
-  }
-
-} // namespace xos
+}  // namespace xos

8571977b958ab109cd980da193a034511cfb4ac1
DavidYeLuo <davidayeluo@gmail.com>
2024-03-13 02:03:30 -0700

    Modify newline handling to not rely on a helper char variable.

--- lexer.cpp
+++ lexer.cpp
@@ -62,19 +62,16 @@ Result<Token> Lexer::LexImpl() {
   }
 
   if (ch == '"') {
+    // FIXME(PiJoules): We should also test for EOF here.
     std::string quoted_str;
     ch = getNextChar();
-    int prev = -1;
     while (ch != '"') {
-      // FIXME(PiJoules): We should also test for EOF here.
-      if (ch == 'n' && prev == '\\') {
+      if (ch == 'n' && !quoted_str.empty() && quoted_str.back() == '\\') {
         quoted_str.back() = '\n';
-        prev = -1;
         ch = getNextChar();
         continue;
       }
       quoted_str += static_cast<char>(ch);
-      prev = ch;
       ch = getNextChar();
     }
     return Token(Token::string, row, col, quoted_str);
